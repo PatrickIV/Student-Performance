d563cd6b92bba2c4ab404c9d612f62654055f82f
Patrick Gatal <62809624+PatrickIV@users.noreply.github.com>
2022-12-09 03:48:43 -0500

    Update ML.py

--- ML.py
+++ ML.py
@@ -7,12 +7,18 @@ from sklearn.utils import shuffle
 data = pd.read_csv('Student-Performance\student-mat.csv', sep = ';')
 
 data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']]
-print(data.head())
 
 predict = 'G3'
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_test, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+print('Coefficient: \n',linear.coef_)
+print('Intercept: \n',linear.intercept_)
