20fc02082bc382edd77de93d2ad3154df64cc09f
Patrick Gatal <62809624+PatrickIV@users.noreply.github.com>
2022-12-09 15:14:57 -0500

    Update ML.py

--- ML.py
+++ ML.py
@@ -16,7 +16,7 @@ predict = 'G3'
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
-'''
+
 best = 0
 for _ in range(30):
         
@@ -31,7 +31,7 @@ for _ in range(30):
         best = acc
         with open('studentmodel.pickle', 'wb') as f:
             pickle.dump(linear, f)
-'''
+
 pickle_in = open('studentmodel.pickle', 'rb')
 linear = pickle.load(pickle_in)
 
